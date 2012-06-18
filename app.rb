46dfd89e6d0cdd64681c8d82d697795f00ec1bce
Rocky Duan <dementrock@gmail.com>
2012-06-18 09:32:37 -0700

    passed spec

--- app.rb
+++ app.rb
@@ -12,8 +12,15 @@ env = env_arg || ENV["SINATRA_ENV"] || "development"
 databases = YAML.load_file("config/database.yml")
 ActiveRecord::Base.establish_connection(databases[env])
 
+class String
+  def is_i?
+    self.to_i.to_s == self
+  end
+end
+
+# retrive all comments of a commentable object
 get '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
-  if commentable_id.to_i.to_s != commentable_id
+  if not commentable_id.is_i?
     error 400, {:error => "commentable_id must be an integer"}.to_json
   else
     comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
@@ -21,8 +28,9 @@ get '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, c
   end
 end
 
+# create a new top-level comment
 post '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
-  if commentable_id.to_i.to_s != commentable_id
+  if not commentable_id.is_i?
     error 400, {:error => "commentable_id must be an integer"}.to_json
   else
     comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
@@ -32,6 +40,27 @@ post '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, 
       comment.to_json
     else
       error 400, comment.errors.to_json
+    end
+  end
+end
+
+# create a new subcomment (reply to comment) only if the comment is NOT a super comment
+post '/api/v1/:commentable_type/:commentable_id/comment/:comment_id' do |commentable_type, commentable_id, comment_id|
+  if not commentable_id.is_i? or not comment_id.is_i?
+    error 400, {:error => "commentable_id and comment_id must be integers"}.to_json
+  else
+    comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+    comment = Comment.find_by_id(comment_id)
+    if comment.nil? or not comment.comment_thread.nil?
+      error 400, {:error => "invalid comment id"}.to_json
+    else
+      comment_params = params.select {|key, value| %w{body title user_id course_id}.include? key}
+      sub_comment = comment.children.create(comment_params)
+      if comment.valid?
+        comment.to_json
+      else
+        error 400, comment.errors.to_json
+      end
     end
   end
 end
