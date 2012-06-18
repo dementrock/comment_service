68889174c257c32f485b795bd37de58b0236cd27
Rocky Duan <dementrock@gmail.com>
2012-06-18 11:35:59 -0700

    resolved conflicts introduced by delete & passed specs

--- app.rb
+++ app.rb
@@ -19,23 +19,42 @@ class String
 end
 
 # retrive all comments of a commentable object
-get '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
-  if not commentable_id.is_i?
-    error 400, {:error => "commentable_id must be an integer"}.to_json
-  else
-    comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
-    comment_thread.json_comments
-  end
+get '/api/v1/commentable/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
+  comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+  comment_thread.json_comments
 end
 
 # create a new top-level comment
-post '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
-  if not commentable_id.is_i?
-    error 400, {:error => "commentable_id must be an integer"}.to_json
+post '/api/v1/commentable/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
+  comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+  comment_params = params.select {|key, value| %w{body title user_id course_id}.include? key}
+  comment = comment_thread.root_comments.create(comment_params)
+  if comment.valid?
+    comment.to_json
   else
-    comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+    error 400, comment.errors.to_json
+  end
+end
+
+# delete a commentable object and its associated comments
+delete '/api/v1/commentable/:commentable_type/:commentable_id' do |commentable_type, commentable_id|
+  comment_thread = CommentThread.find_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+  if comment_thread.nil?
+    error 400, {:error => "commentable object does not exist"}.to_json
+  else
+    comment_thread.destroy
+    comment_thread.to_json
+  end
+end
+
+# create a new subcomment (reply to comment) only if the comment is NOT a super comment
+post '/api/v1/comment/:comment_id' do |comment_id|
+  comment = Comment.find_by_id(comment_id)
+  if comment.nil? or not comment.comment_thread.nil?
+    error 400, {:error => "invalid comment id"}.to_json
+  else
     comment_params = params.select {|key, value| %w{body title user_id course_id}.include? key}
-    comment = comment_thread.root_comments.create(comment_params)
+    sub_comment = comment.children.create(comment_params)
     if comment.valid?
       comment.to_json
     else
@@ -44,22 +63,12 @@ post '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, 
   end
 end
 
-# create a new subcomment (reply to comment) only if the comment is NOT a super comment
-post '/api/v1/comment/:comment_id' do |comment_id|
-  if not comment_id.is_i?
-    error 400, {:error => "comment_id must be integers"}.to_json
+delete '/api/v1/comment/:comment_id' do |comment_id|
+  comment = Comment.find_by_id(comment_id)
+  if comment.nil?
+    error 400, {:error => "comment does not exist"}.to_json
   else
-    comment = Comment.find_by_id(comment_id)
-    if comment.nil? or not comment.comment_thread.nil?
-      error 400, {:error => "invalid comment id"}.to_json
-    else
-      comment_params = params.select {|key, value| %w{body title user_id course_id}.include? key}
-      sub_comment = comment.children.create(comment_params)
-      if comment.valid?
-        comment.to_json
-      else
-        error 400, comment.errors.to_json
-      end
-    end
+    comment.destroy
+    comment.to_json
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'yajl'
 require 'pp'
+use Rack::MethodOverride
 
 describe "app" do
   before :each do
@@ -9,25 +10,21 @@ describe "app" do
   end
   describe "create empty thread on request" do
     it "should create a corresponding comment thread with a super comment" do
-      get "/api/v1/questions/1/comments"
+      get "/api/v1/commentable/questions/1/comments"
       last_response.should be_ok
       comment_thread = CommentThread.first
       comment_thread.should_not be_nil
       comment_thread.super_comment.should_not be_nil
     end
     it "should create a corresponding comment thread with correct type and id" do
-      get "/api/v1/questions/1/comments"
+      get "/api/v1/commentable/questions/1/comments"
       last_response.should be_ok
       comment_thread = CommentThread.first
       comment_thread.commentable_type.should == 'questions'
       comment_thread.commentable_id.should == '1'
     end
-    it "should return a 400 bad request if commentable_id is not an integer" do
-      get "/api/v1/questions/a_question/comments"
-      last_response.status.should == 400
-    end
     it "should return an empty json when there are no comments" do
-      get "/api/v1/questions/1/comments"
+      get "/api/v1/commentable/questions/1/comments"
       last_response.should be_ok
       attributes = JSON.parse(last_response.body)
       attributes.length.should == 0
@@ -35,7 +32,7 @@ describe "app" do
   end
   describe "create top-level comments" do
     it "should create a top-level comment with correct body, title, user_id, and course_id" do
-      post "/api/v1/questions/1/comments", :body => "comment body", :title => "comment title", :user_id => 1, :course_id => 1
+      post "/api/v1/commentable/questions/1/comments", :body => "comment body", :title => "comment title", :user_id => 1, :course_id => 1
       last_response.should be_ok
       comment = CommentThread.first.root_comments.first
       comment.should_not be_nil
@@ -69,7 +66,7 @@ describe "app" do
   end
   describe "retrieve comments" do
     it "returns an empty array when there are no comments" do
-      get "/api/v1/questions/1/comments"
+      get "/api/v1/commentable/questions/1/comments"
       last_response.should be_ok
       comments = Yajl::Parser.parse last_response.body
       comments.length.should == 0
@@ -82,7 +79,7 @@ describe "app" do
       sub_comment << (comment[0].children.create :body => "comment body", :title => "comment title 0", :user_id => 1, :course_id => 1)
       comment << (comment_thread.root_comments.create :body => "top comment", :title => "top 1", :user_id => 1, :course_id => 1)
       sub_comment << (comment[1].children.create :body => "comment body", :title => "comment title 1", :user_id => 1, :course_id => 1)
-      get "/api/v1/questions/1/comments"
+      get "/api/v1/commentable/questions/1/comments"
       last_response.should be_ok
       comments = Yajl::Parser.parse last_response.body
       comments.length.should == 2
@@ -108,23 +105,24 @@ describe "app" do
       sub_comment << (comment[1].children.create :body => "comment body", :title => "comment title 1", :user_id => 1, :course_id => 1)
     end
     it "should return error when called on a nonexisted thread" do
-      delete "/api/v1/i_do_not_exist/1"
+      delete "/api/v1/commentable/i_do_not_exist/1"
       last_response.status.should == 400
     end
     it "deletes all comments associated with a thread when called on the thread" do
-      delete "/api/v1/questions/1"      
+      delete "/api/v1/commentable/questions/1"      
       last_response.should be_ok
       CommentThread.count.should == 0
       Comment.count.should == 0
     end
     it "deletes the comment and all sub comments when called on the comment" do
       comment_thread = CommentThread.first
-      comment = comment_thread.comments.first
+      comment = comment_thread.root_comments.first
       delete "/api/v1/comment/#{comment.id}"
-      CommentThread.root_comments.count.should == 1
-      CommentThread.comments.count.should == 2
-      CommentThread.root_comments.first.title.should == "top 1"
-      CommentThread.root_comments.first.children.first.title.should == "comment title 1"
+      last_response.should be_ok
+      comment_thread.root_comments.count.should == 1
+      comment_thread.comments.count.should == 2
+      comment_thread.root_comments.first.title.should == "top 1"
+      comment_thread.root_comments.first.children.first.title.should == "comment title 1"
     end
   end
 end
