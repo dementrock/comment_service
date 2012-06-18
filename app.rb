c0c1ab0b7633d25533fe69d97ca5a800764d8769
Rocky Duan <dementrock@gmail.com>
2012-06-18 08:34:30 -0700

    passed specs

--- app.rb
+++ app.rb
@@ -6,25 +6,17 @@ require 'sinatra'
 require_relative 'models/comment'
 require_relative 'models/comment_thread'
 
+env_index = ARGV.index("-e")
+env_arg = ARGV[env_index + 1] if env_index
+env = env_arg || ENV["SINATRA_ENV"] || "development"
+databases = YAML.load_file("config/database.yml")
+ActiveRecord::Base.establish_connection(databases[env])
 
-class Service < Sinatra::Base
-  configure do
-    env = ENV["SINATRA_ENV"] || "development"
-    databases = YAML.load_file("config/database.yml")
-    ActiveRecord::Base.establish_connection(databases[env])
+get '/api/v1/:commentable_type/:commentable_id/comments' do |commentable_type, commentable_id|
+  if commentable_id.to_i.to_s == commentable_id
+    comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id(commentable_type, commentable_id)
+    comment_thread.json_comments
+  else
+    error 400, {:error => "commentable_id must be an integer"}.to_json
   end
-
-  before do
-    content_type :json
-  end
-
-  get '/api/v1/:commentable_type/:commentable_id/comments' do
-    if params[:commentable_id].to_i.to_s == params[:commentable_id]
-      comment_thread = CommentThread.find_or_create_by_commentable_type_and_commentable_id params[:commentable_type], params[:commentable_id]
-      comment_thread.json_comments
-    else
-      error 400, {:error => "commentable_id must be an integer"}.to_json
-    end
-  end
-
 end
